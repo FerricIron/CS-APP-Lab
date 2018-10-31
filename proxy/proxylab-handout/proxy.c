a74982b8d8c4f302e210d23b3f374f7c8e0c2375
lengyu <lengyuchn@qq.com>
2018-10-31 15:44:38 +0800

    finish part I

--- proxy/proxylab-handout/proxy.c
+++ proxy/proxylab-handout/proxy.c
@@ -8,7 +8,6 @@
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 static const char *user_connection="Connection: close\r\n";
 static const char *user_proxy_connection="Proxy-Connection: close\r\n";
-static const char *user_host="Host: www.cmu.edu\r\n";
 typedef struct {
 	char user_agent_hdr[MAXLINE];
 	char user_connection[MAXLINE];
@@ -21,6 +20,7 @@ void ServerError(int fd, char *cause, char status[], char *shortMsg, char *longM
 void ServerPrintResponseHeader(char *header);
 int getRequestType(char *buf);
 void initHeader(Headers* header);
+void ParseUrl(char url[],char uri[],char hostname[],char port[],Headers* header);
 void GetRequestHeaders(rio_t *rio,char requestHeaders[],Headers *header);
 int main(int argc,char **argv)
 {
@@ -50,22 +50,20 @@ void todo(int fd)
 {
 	rio_t rio,rioc;
 	int rc;
-	char buf[MAXLINE],method[MAXLINE],uri[MAXLINE],version[MAXLINE];
+	char buf[MAXLINE],method[MAXLINE],uri[MAXLINE],version[MAXLINE],url[MAXLINE];
 	char hostname[MAXLINE],port[MAXLINE],temp[MAXLINE];
 	Rio_readinitb(&rio,fd);
 	Rio_readlineb(&rio,buf,MAXLINE);
-	sscanf(buf,"%s %s %s",method,uri,version);
-//	fprintf(stdout, "%s\n",buf );
-//	printf("here\n");
+  fprintf(stdout,"Proxy recive header:\n%s\n",buf);
+	sscanf(buf,"%s %s %s",method,url,version);
 	if(strcasecmp(method,"GET"))
 	{
 		ServerError(fd,method,"501","Not implemented","ProxyLab not implement this method\n");
 	}
+  Headers header;
+  ParseUrl(url,uri,hostname,port,&header);
 	sprintf(buf,"%s %s %s",method,uri,"HTTP/1.0\r\n");
-	//printf("here\n");
-	Headers header;
 	GetRequestHeaders(&rio,buf,&header);
-//	fprintf(stdout, "%s\n",buf );
 	sscanf(header.user_host,"%s %s",temp,hostname);
 	char *ptr=strchr(hostname,':');
 	if(ptr!=NULL){
@@ -75,27 +73,28 @@ void todo(int fd)
 	{
 		strcpy(port,"80");
 	}
-	//fprintf(stdout, "%s:%s\n",hostname,port);
+  fprintf(stdout,"Open_clientfd:hostname=%s port=%s\n",hostname,port);
 	int connfd=Open_clientfd(hostname,port);
-	//fprintf(stdout, "%d\n",connfd );
 	if(connfd<0)
 	{
 		ServerError(fd,method,"500","Can not Connection","Proxy Can't connection to end server\n");
 	}
+  fprintf(stdout,"Proxy will send header:\n%s\n",buf);
 	Rio_writen(connfd,buf,MAXLINE);
-	//fprintf(stdout,"requestHeaders:%s",buf);
 	Rio_readinitb(&rioc,connfd);
 	while((rc=Rio_readlineb(&rioc,buf,MAXLINE))>0)
 	{
-		//fprintf(stdout, "Response:%s\n",buf );
-		Rio_writen(fd,buf,MAXLINE);
+    fprintf(stdout,"Proxy received %d bytes,ten send to end_server\n",rc);
+		Rio_writen(fd,buf,rc);
 	}
+  Close(connfd);
 	return ;
 }
 int getRequestType(char *buf)
 {
 	const static char type[MAX_TYPE_SIZE][MAXLINE]={"Host:","User-Agent:","Connection:","Proxy-Connection:"};
-	for(int i=0;i<MAX_TYPE_SIZE;++i)
+	int i;
+  for(i=0;i<MAX_TYPE_SIZE;++i)
 	{
 		if(strcasecmp(buf,type[i])==0)
 		{
@@ -104,12 +103,28 @@ int getRequestType(char *buf)
 	}
 	return -1;
 }
+void ParseUrl(char url[],char uri[],char hostname[],char port[],Headers* header)
+{
+  char *ptr=strchr(url,':');
+  char *uri_ptr=strchr(ptr+3,'/');
+  strcpy(uri,uri_ptr);
+  *uri_ptr='\0';
+  strcpy(header->user_host,ptr+2);
+  char *port_ptr=strchr(ptr+3,':');
+  if(port_ptr==NULL)
+    {
+      strcpy(port,"80");
+    }else{
+      strcpy(port,port_ptr+1);
+      *port_ptr='\0';
+   }
+  strcpy(hostname,ptr+3);
+}
 void initHeader(Headers* header)
 {
 	strcpy(header->user_agent_hdr,user_agent_hdr);
 	strcpy(header->user_connection,user_connection);
 	strcpy(header->user_proxy_connection,user_proxy_connection);
-	strcpy(header->user_host,user_host);
 }
 void GetRequestHeaders(rio_t *rio,char requestHeaders[],Headers *header)
 {
@@ -170,7 +185,7 @@ void GetRequestHeaders(rio_t *rio,char requestHeaders[],Headers *header)
 
 void PrintErrorHeader(int fd,char status[],char* shortMsg)
 {
-    char filetype[MAXLINE], buf[MAXBUF];
+    char buf[MAXBUF];
     /*Send the HTTP response Header to client*/
     sprintf(buf, "HTTP/1.0 %s %s\r\n",status,shortMsg);
     sprintf(buf, "%sServer: Yu Web Server\r\n", buf);
