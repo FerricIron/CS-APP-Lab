fdcf37e72fbb763ceb0f8a4809530436902a89fe
lengyu <lengyuchn@qq.com>
2018-11-14 20:32:20 +0800

    syn

--- malloc/malloclab-handout/mm.c
+++ malloc/malloclab-handout/mm.c
@@ -24,16 +24,18 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "SaltedFish",
     /* First member's full name */
-    "Harry Bovik",
+    "Yu.Leng",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "lengyuchn@gmail.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
     ""
 };
+
+
 
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
